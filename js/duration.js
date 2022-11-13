3fbbf869d0f99943b93ce46891533a5db1972a82
A1eeeee <yueqiang1999@outlook.com>
2022-11-14 07:11:59 +0800

    Site updated: 2022-11-14 07:11:58

---
+++ js/duration.js
@@ -0,0 +1,56 @@
+!(function() {
+    var start = new Date("2022/03/03 00:00:00");
+  
+    function update() {
+      var now = new Date();
+      now.setTime(now.getTime()+250);
+      days = (now - start) / 1000 / 60 / 60 / 24;
+      dnum = Math.floor(days);
+      hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+      hnum = Math.floor(hours);
+      if(String(hnum).length === 1 ){
+        hnum = "0" + hnum;
+      }
+      minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+      mnum = Math.floor(minutes);
+      if(String(mnum).length === 1 ){
+        mnum = "0" + mnum;
+      }
+      seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+      snum = Math.round(seconds);
+      if(String(snum).length === 1 ){
+        snum = "0" + snum;
+      }
+      document.getElementById("timeDate").innerHTML = "本站存活&nbsp"+dnum+"&nbsp天";
+      document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    }
+  
+    update();
+    setInterval(update, 1000);
+  })();
+  hexo.extend.injector.register('body_end', function () {
+    const {
+        enable,
+        btn_icon,
+        btn_text,
+        comment,
+        qrcodes,
+    } = hexo.config.reward_button;
+
+    if (!enable) {
+        return null;
+    }
+
+    return `
+    <link defer rel="stylesheet" href="/css/reward-button.css"/>
+    <script src="/js/reward-button.js"></script>
+    <script>
+        new RewardButton({
+            btnIcon: ${btn_icon ? `"${btn_icon}"` : "null"},
+            btnText: "${btn_text}",
+            comment: "${comment}",
+            qrcodes: ${JSON.stringify(qrcodes)}
+        }).init();
+    </script>
+    `
+}, "post");
